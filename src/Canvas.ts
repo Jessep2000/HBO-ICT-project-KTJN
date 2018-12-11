de2de151511bfcf3ec8e4c9798eca2c4f901e610
ItzzMystery <nickk0164@hotmail.com>
2018-12-11 12:31:31 +0100

    v0.0.12 *add methods to Canvas.ts

--- src/Canvas.ts
+++ src/Canvas.ts
@@ -12,12 +12,27 @@ class Canvas
         this.canvas.getContext("2d");
     }
 
-    public drawTextToCanvas(
+    /**
+     * writeTextToCanvas
+     * @accessModifier {public}
+     * @param {string} text - 
+     * @param {number} fontSize - 
+     * @param {number} xPos -
+     * @param {number} yPos -
+     * @param {string} color -
+     * @param {CanvasTextAlign} alignment -
+     */
+    public writeTextToCanvas(
         text: string,
         fontSize: number,
         xPos: number,
-        yPos: null
+        yPos: number,
+        color: string = "white",
+        alignment: CanvasTextAlign = "center"
     ): void {
-
+        this.ctx.font = `${fontSize}px Supersonic Rocketship`;
+        this.ctx.fillStyle = color;
+        this.ctx.textAlign = alignment;
+        this.ctx.fillText(text, xPos, yPos)
     }
 }
--- src/Timer.ts
+++ src/Timer.ts
@@ -1,27 +1,37 @@
+//NOT SURE IF THIS IS GOING TO WORK AFTER ALL
 class Timer
 {
-    private _element: any;
+    private _element: Canvas;
     private _endTime: any;
     //private _hours: number;        [OPTIONAL]
-    private _mins: number;
+    private _minutes: number;
+    private _seconds: number
     private _msLeft: number;
     private _time: any;
-    
-    private _canvas: Canvas;
 
-    public constructor(min: number, sec: number)
+    public constructor(mins: number, secs: number)
     {
         
     }
 
-    public twoDigits(n: any): number {
+    public twoDigits(n: number) {
         return(n <= 9 ? "0" + n : n);
     };
 
     public updateTimer(): void {
         this._msLeft = this._endTime - (+new Date);
         if (this._msLeft < 1000) {
-            this._canvas.drawTextToCanvas("Time's up")
+            this._element.writeTextToCanvas("Time's up", 20, 50, 50, "black");
+        } else {
+            this._time = new Date(this._msLeft);
+            this._minutes = this._time.getUTCMinutes();
+            this._seconds = this._time.getUTCSeconds();
+            //TODO: add writeTimerToCanvas
+            setTimeout(this.updateTimer, this._time.getUTCMiliseconds() + 500 );
         }
     }
+
+    element = document.getElementById("canvas");
+    endTime = (+new Date) + 1000 * (60*this._minutes + this._seconds) + 500;
+    //TODO: call the updateTimer() method here {ISN'T WORKING YET}
 }
