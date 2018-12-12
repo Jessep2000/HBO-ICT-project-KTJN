eff49625a7dc9eb16a5d4c2a1b4de321195c8688
KevinVeenstra <42801649+KevinVeenstra@users.noreply.github.com>
2018-12-12 11:05:05 +0100

    v0.1.1 start highscores

--- src/Higscores.ts
+++ src/Higscores.ts
@@ -1,12 +1,33 @@
 class Highscores {
 
-    private readonly score : number;
+    private readonly canvas: HTMLCanvasElement;
+    private readonly ctx: CanvasRenderingContext2D;
 
-    public constructor (
+    private readonly playerName: string;
+    private readonly score: number;
+    private readonly highscores: Array<any>;
 
-    ) {
-        
+    public constructor(canvasId: HTMLCanvasElement, playerName: string, score: number ) {
+        this.canvas = canvasId;
+        this.playerName = playerName;
+        this.score = score;
+        this.highscores = [
+            {
+                playerName: 'Kevin',
+                score: 60
+            },
+            {
+                playerName: 'Jesse',
+                score: 50
+            },
+            {
+                playerName: 'Tijs',
+                score: 40
+            },
+            {
+                playerName: 'Nick',
+                score: 20
+            }
+        ]
     }
-
-
 }
