ea9b1fe9d3bee0b3083e634a742e0c30bb7d8ff3
Jessep2000 <42765970+Jessep2000@users.noreply.github.com>
2018-12-12 11:12:53 +0100

    v0.1.0a

--- src/Game.ts
+++ src/Game.ts
@@ -11,17 +11,24 @@ class Game {
 }
 
 let init = function () {
+    // const Deliveration = new Game()
     var level1: Array<string> = [
         '3_270_t_split', 'grass', '2_0_turn', '2_0_straight', '2_90_turn',
         '2_90_turn', '2_0_straight', '4_x_split', '2_90_turn', '2_90_turn',
-        '1_90_turn', '1_90_turn', '4_270_t_split', '1_90_turn', '1_90_turn',
-        '1_90_turn', '1_90_turn', '1_90_turn', '1_90_turn', '1_90_turn',
-        '1_90_turn', '1_90_turn', '1_90_turn', '1_90_turn', '1_90_turn'
+        '2_90_turn', '2_90_turn', '4_270_t_split', '2_90_turn', '2_90_turn',
+        '2_90_turn', '2_90_turn', 'grass', '2_x_split', '2_90_turn',
+        '2_90_turn', '2_90_turn', '2_90_turn', '2_90_turn', '3_90_straight'
     ]
-    var level: Level = new Level(5, level1, canvas)
+    var Loadlevel1: Level = new Level(5, level1, canvas)
 
-    level.writeLevel();
-    const Deliveration = new Game()
+    // var level2: Array<string> = [
+
+    // ]
+
+    // var loadlevel2: Level = new Level(5, level2, canvas)
+
+    Loadlevel1.init();
+
 }
 
 window.addEventListener("load", init);
