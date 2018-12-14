0c639dc73391381058b7a94e5cf05b5283a2fda5
ItzzMystery <nickk0164@hotmail.com>
2018-12-14 16:38:57 +0100

    V0.1.7 *making start with Entity.ts

--- src/entity/Entity.ts
+++ src/entity/Entity.ts
@@ -1,24 +1,18 @@
 class Entity
 {
-    private imageSource: string;
+    private readonly imageSource: string;
+    private readonly canvas: Canvas;
     private xPos: number;
     private yPos: number;
     private width: number;
-    private height: number; 
+    private height: number;
 
-    protected _canvas: Canvas;
-
-    public constructor(imgSrc: string, xCoor: number, yCoor: number, width: number, height: number, canvas: HTMLCanvasElement) {
-        this.imageSource = imgSrc;
+    public constructor(imageSrc: string, xCoor: number, yCoor: number, width: number, height: number, canvas: HTMLCanvasElement) {
+        this.imageSource = imageSrc;
         this.xPos = xCoor;
         this.yPos = yCoor;
         this.width = width;
         this.height = height;
-        // this._canvas = canvas
-    }
-
-    //Draw bus
-    public drawBus(): void {
-        //this._canvas.writeImageToCanvas(/*TODO: add missing arguments*/)
+        //this.canvas = new Canvas(canvas /*Need 2 more arguments*/);
     }
 }
